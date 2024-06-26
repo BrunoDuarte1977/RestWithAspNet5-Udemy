a0f3c5b97221ecb1b1de47646c6b0da3fe2657d3
Bruno Duarte <brunoduarte1977@gmail.com>
2024-06-26 15:40:36 -0300

    Asp.Versioning.Mvc

---
+++ 05_RestWithASPNetUdemy_Versioning/RestWithASPNetUdemy/RestWithASPNetUdemy/Services/IPersonService.cs
@@ -0,0 +1,17 @@
+﻿using RestWithASPNetUdemy.Model;
+
+namespace RestWithASPNetUdemy.Services
+{
+    public interface IPersonService
+    {
+        Person Create(Person person);
+
+        Person Update(Person person);
+
+        void Delete(long id);
+
+        Person FindById(long id);
+
+        List<Person> FindAll();
+    }
+}
