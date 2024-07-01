68cf0061d46385a0d54cb3ea9fb07bb313f5f871
Bruno Duarte <brunoduarte1977@gmail.com>
2024-07-01 14:33:40 -0300

    Working with value object (VO)

---
+++ 09_RestWithASPNetUdemy_ValueObject_VO/RestWithASPNetUdemy/RestWithASPNetUdemy/Business/IBookService.cs
@@ -0,0 +1,17 @@
+﻿using RestWithASPNetUdemy.Model;
+
+namespace RestWithASPNetUdemy.Business
+{
+    public interface IBookBusiness
+    {
+        BookVO Create(BookVO book);
+
+        BookVO Update(BookVO book);
+
+        void Delete(long id);
+
+        BookVO FindById(long id);
+
+        List<BookVO> FindAll();
+    }
+}
---
+++ 09_RestWithASPNetUdemy_ValueObject_VO/RestWithASPNetUdemy/RestWithASPNetUdemy/Program.cs
@@ -0,0 +1,66 @@
+using Microsoft.EntityFrameworkCore;
+using RestWithASPNetUdemy.Model.Context;
+using RestWithASPNetUdemy.Business;
+using RestWithASPNetUdemy.Business.Implementations;
+using RestWithASPNetUdemy.Repository;
+using EvolveDb;
+using MySqlConnector;
+using Serilog;
+using RestWithASPNetUdemy.Repository.Generic;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+//DB Connection
+var connection = builder.Configuration["MySQLConnection:MySQLConnectionString"];
+builder.Services.AddDbContext<MySQLContext>(options => options.UseMySql(
+    connection,
+    new MySqlServerVersion(new Version(8, 4, 0)))
+);
+
+if (builder.Environment.IsDevelopment())
+{
+    MigrateDatabase(connection);
+}
+
+//Versioning API
+builder.Services.AddApiVersioning();
+
+//Dependency Injection
+builder.Services.AddScoped<IPersonBusiness, PersonBusinessImplementation>();
+builder.Services.AddScoped<IBookBusiness, BookBusinessImplementation>();
+builder.Services.AddScoped(typeof(IRepository<>),typeof(GenericRepository<>));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
+void MigrateDatabase(string connection)
+{
+    try
+    {
+        var evolveConnection = new MySqlConnection(connection);
+        var evolve = new Evolve(evolveConnection, Log.Information)
+        {
+            Locations = new List<string> { "db/migrations", "db/dataset" },
+            IsEraseDisabled = true,
+        };
+        evolve.Migrate();
+    }
+    catch (Exception ex)
+    {
+        Log.Error("Database migration failed", ex);
+        throw;
+    }
+}
