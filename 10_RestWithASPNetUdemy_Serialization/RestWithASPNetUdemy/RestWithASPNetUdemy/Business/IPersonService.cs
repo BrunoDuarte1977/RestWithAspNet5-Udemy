4cb0a83c9763802534499731cf2d2777405ec7bc
Bruno Duarte <brunoduarte1977@gmail.com>
2024-07-03 11:25:41 -0300

    Working with serialization

---
+++ 10_RestWithASPNetUdemy_Serialization/RestWithASPNetUdemy/RestWithASPNetUdemy/Business/IPersonService.cs
@@ -0,0 +1,18 @@
+﻿using RestWithASPNetUdemy.Data.VO;
+using RestWithASPNetUdemy.Model;
+
+namespace RestWithASPNetUdemy.Business
+{
+    public interface IPersonBusiness
+    {
+        PersonVO Create(PersonVO person);
+
+        PersonVO Update(PersonVO person);
+
+        void Delete(long id);
+
+        PersonVO FindById(long id);
+
+        List<PersonVO> FindAll();
+    }
+}
---
+++ 10_RestWithASPNetUdemy_Serialization/RestWithASPNetUdemy/RestWithASPNetUdemy/Repository/Generic/IRepository .cs
@@ -0,0 +1,20 @@
+﻿using RestWithASPNetUdemy.Model;
+using RestWithASPNetUdemy.Model.Base;
+
+namespace RestWithASPNetUdemy.Repository
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        T Create(T item);
+
+        T Update(T item);
+
+        void Delete(long id);
+
+        T FindById(long id);
+
+        List<T> FindAll();
+
+        bool Exists(long id);
+    }
+}
